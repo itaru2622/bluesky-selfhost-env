85c9aa2808b8784a7128b4bf5d05185d9c0c90bb
itaru2622 <itaru2622@gmail.com>
2024-03-04 21:58:55 +0900

    update: ops/api-bsky.mk, a little.

--- ops/api-bsky.mk
+++ ops/api-bsky.mk
@@ -2,12 +2,12 @@
 # output file path of API response.
 resp ?=/dev/null
 
-#HINT: make api_CreateAccount email=...  password=...  handle=...  (cf. handle: DOMAIN will be appended in auto.)
-api_CreateAccount:	_mkmsg_createAccount  _sendMsg
+#HINT: make api_CreateAccount email=...  password=...  handle=...
+api_CreateAccount:: _mkmsg_createAccount  _sendMsg
+api_CreateAccount:: _echo_reqAccount _findDid
 
 #HINT: make api_CreateAccount_feedgen
-api_CreateAccount_feedgen: getFeedgenUserinfo _mkmsg_createAccount _sendMsg _echo_reqAccount
-	-cat ${resp} | jq .did | sed 's/"//g'
+api_CreateAccount_feedgen: getFeedgenUserinfo api_CreateAccount
 
 _sendMsg:
 	@curl -L -X ${method} ${url} ${header} ${msg} | tee -a ${resp}
@@ -16,15 +16,16 @@ _mkmsg_createAccount:
 	$(eval url='https://pds.${DOMAIN}/xrpc/com.atproto.server.createAccount')
 	$(eval method=POST)
 	$(eval header=-H 'Content-Type: application/json'  -H 'Accept: application/json')
-	$(eval msg=-d '{ "email": "${email}" ,"handle": "${handle}.${DOMAIN}", "password": "${password}" }')
+	$(eval msg=-d '{ "email": "${email}" ,"handle": "${handle}", "password": "${password}" }')
 
 getFeedgenUserinfo:
-	$(eval handle=feedgen)
-	$(eval email=no-reply-${handle}@${DOMAIN})
-	$(eval password=`cat ${passfile} | grep FEEDGENERATOR_PASSWORD | sed 's/.*=//'`)
+	$(eval handle=${FEEDGEN_PUBLISHER_HANDLE})
+	$(eval email=${FEEDGENERATOR_EMAIL})
+	$(eval password=$(shell cat ${passfile} | grep FEEDGEN_PUBLISHER_PASSWORD | awk -F= '{ print $$2}'))
 	$(eval resp=${aDir}/${handle}.secrets)
 
 _echo_reqAccount:
+	@echo ""
 	@echo "handle:     ${handle}"
 	@echo "email:      ${email}"
 	@echo "password:   ${password}"
@@ -35,3 +36,7 @@ _echo_apiops:
 	@echo "method: ${method}"
 	@echo "header: ${header}"
 	@echo "msg:    ${msg}"
+
+_findDid:
+	@echo -n "### DID: "
+	-@cat ${resp} | jq .did | sed 's/"//g'
