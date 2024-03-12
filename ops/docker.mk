fe46f6cc534e2bcffd7c32330969b06a51240ab2
itaru2622 <itaru2622@gmail.com>
2024-03-12 18:50:54 +0900

    update ops/docker.mk to feed new BSKY_SERVICE_SIGNING_KEY to docker-compose.

--- ops/docker.mk
+++ ops/docker.mk
@@ -1,8 +1,52 @@
 
+#
+# _TEMPLATE_VARS: make lists of env vars to feed docker-compose and containers >>>>>
+#
+define _TEMPLATE_VARS
+DOMAIN=${DOMAIN} \
+asof=${asof} \
+dDir=${dDir} \
+LOG_LEVEL_DEFAULT=${LOG_LEVEL_DEFAULT} \
+EMAIL4CERTS=${EMAIL4CERTS} \
+PDS_EMAIL_SMTP_URL=${PDS_EMAIL_SMTP_URL} \
+FEEDGEN_PUBLISHER_DID=${FEEDGEN_PUBLISHER_DID} \
+FEEDGEN_PUBLISHER_HANDLE=${FEEDGEN_PUBLISHER_HANDLE} \
+FEEDGEN_PUBLISHER_PASSWORD=${FEEDGEN_PUBLISHER_PASSWORD} \
+ADMIN_PASSWORD=${ADMIN_PASSWORD} \
+BGS_ADMIN_KEY=${BGS_ADMIN_KEY} \
+IMG_URI_KEY=${IMG_URI_KEY} \
+IMG_URI_SALT=${IMG_URI_SALT} \
+MODERATOR_PASSWORD=${MODERATOR_PASSWORD} \
+OZONE_ADMIN_PASSWORD=${OZONE_ADMIN_PASSWORD} \
+OZONE_MODERATOR_PASSWORD=${OZONE_MODERATOR_PASSWORD} \
+OZONE_SIGNING_KEY_HEX=${OZONE_SIGNING_KEY_HEX} \
+OZONE_TRIAGE_PASSWORD=${OZONE_TRIAGE_PASSWORD} \
+PDS_ADMIN_PASSWORD=${PDS_ADMIN_PASSWORD} \
+PDS_JWT_SECRET=${PDS_JWT_SECRET} \
+PDS_PLC_ROTATION_KEY_K256_PRIVATE_KEY_HEX=${PDS_PLC_ROTATION_KEY_K256_PRIVATE_KEY_HEX} \
+PDS_REPO_SIGNING_KEY_K256_PRIVATE_KEY_HEX=${PDS_REPO_SIGNING_KEY_K256_PRIVATE_KEY_HEX} \
+SERVICE_SIGNING_KEY=${SERVICE_SIGNING_KEY} \
+TRIAGE_PASSWORD=${TRIAGE_PASSWORD} \
+POSTGRES_PASSWORD=${POSTGRES_PASSWORD} \
+BSKY_SERVICE_SIGNING_KEY=${BSKY_SERVICE_SIGNING_KEY} \
+PASS=${PASS}
+endef
+export _TEMPLATE_VARS
+#
+# _TEMPLATE_VARS: make lists of env vars to feed docker-compose and containers <<<<<
+#
+
+_up_templ: _load_vars
+	${_envs} docker-compose -f ${f} up -d ${services}
+_load_vars:
+	$(eval _envs=$${_TEMPLATE_VARS})
+	@echo ${_envs} | sed 's/ /\n/g' | awk -F= '{print $$1,"=",$$2}' | sed 's/ //g'
+
 build:
 	DOMAIN=${DOMAIN} asof=${asof} docker-compose -f ${f} build ${services}
 
-docker-start::      setupdir config/caddy/Caddyfile certs/ca-certificates.crt ${passfile} _applySdep _docker_up
+#docker-start::      setupdir config/caddy/Caddyfile certs/ca-certificates.crt ${passfile} _applySdep _docker_up
+docker-start::      setupdir config/caddy/Caddyfile certs/ca-certificates.crt ${passfile} _applySdep _up_templ
 docker-start::      docker-watchlog
 docker-start-bsky:: _applySbsky _docker_up
 docker-start-bsky:: docker-watchlog
@@ -42,6 +86,7 @@ _docker_up:
 	    SERVICE_SIGNING_KEY=${SERVICE_SIGNING_KEY} \
 	    TRIAGE_PASSWORD=${TRIAGE_PASSWORD} \
 	    POSTGRES_PASSWORD=${POSTGRES_PASSWORD} \
+	    BSKY_SERVICE_SIGNING_KEY=${BSKY_SERVICE_SIGNING_KEY} \
 	    PASS=${PASS} \
         docker-compose -f ${f} up -d ${services}
 
