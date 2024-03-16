026a08a52550df43c83ea493e7036a53bca73c92
itaru2622 <itaru2622@gmail.com>
2024-03-16 16:28:53 +0900

    add: patch applyer on atproto for docker build stage, in ops/patch.mk.

--- ops/patch.mk
+++ ops/patch.mk
@@ -1,7 +1,7 @@
 # variable to specify branch to apply patch. as below, <empty> then apply patch to current branch.
 branch2patch=
 
-patch-dockerbuild: ${rDir}/feed-generator/.dockerbuild  ${rDir}/indigo/.dockerbuild
+patch-dockerbuild: ${rDir}/feed-generator/.dockerbuild  ${rDir}/indigo/.dockerbuild  ${rDir}/atproto/.dockerbuild
 
 ${rDir}/feed-generator/.dockerbuild:
 	@echo "make branch and applying patch..."
@@ -17,6 +17,12 @@ ${rDir}/indigo/.dockerbuild:
 	touch $@
 	(cd ${rDir}/indigo; git add . ; git commit -m "update: dockerbuild"; )
 
+${rDir}/atproto/.dockerbuild:
+	@echo "make branch and applying patch..."
+	(cd ${rDir}/atproto; git status; git checkout ${branch2patch} -b dockerbuild )
+	for ops in `ls ${wDir}/patching/1*.sh | grep atproto`; do wDir=${wDir} rDir=${rDir} pDir=${wDir}/patching DOMAIN=${DOMAIN} asof=${asof}  $${ops} ; done
+	touch $@
+	(cd ${rDir}/atproto; git add . ; git commit -m "update: dockerbuild"; )
 
 patch-selfhost: ${rDir}/social-app/.selfhost-${DOMAIN} ${rDir}/feed-generator/.selfhost-${DOMAIN}
 ${rDir}/social-app/.selfhost-${DOMAIN}::
