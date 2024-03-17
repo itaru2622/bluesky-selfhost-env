a7a0b2a001626d4e6a602f7a2693425486ede08b
itaru2622 <itaru2622@gmail.com>
2024-03-18 08:23:52 +0900

    update: selfhosting endpoint for public.api.bsky.app

---
+++ patching/300-social-app-selfhost-more.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/bash
+
+echo "DOMAIN: ${DOMAIN}"
+echo "rDir:   ${rDir}"
+
+pushd ${rDir}/social-app/src
+
+# starts patch, more           >>>>>>>>>>
+
+# for no-auth API call, cf. https://github.com/bluesky-social/bsky-docs/issues/63
+f=./state/queries/index.ts
+if [ -f $f ] && [ -n "`grep -R public.api.bsky.app $f`" ];then
+	sed -i "s/public.api.bsky.app/api.${DOMAIN}/g" $f
+	echo "public.api.bsky.app => public.api.${DOMAIN}  for $f"
+fi
+
+popd
+
+# starts patch, more           <<<<<<<<<<
