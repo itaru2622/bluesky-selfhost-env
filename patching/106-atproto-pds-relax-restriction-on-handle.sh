d99c46b9313242db129c1f75b33a43cd99759159
itaru2622 <itaru2622@gmail.com>
2024-04-11 16:59:44 +0900

    update: re-implementing patch for relaxing restriction on pds handle.length to follow discussion in bluesky-social/atproto

--- patching/106-atproto-pds-relax-restriction-on-handle.sh
+++ patching/106-atproto-pds-relax-restriction-on-handle.sh
@@ -8,8 +8,8 @@ pushd ${rDir}/atproto
 
 f=./packages/pds/src/handle/index.ts
 if [ -f $f ] && [ -n "`grep 'handle.length > 30' $f`" ];then
-	sed -i "s#handle.length > 30#handle.length > 255#" $f
-	echo "relax restriction on pds handle.length : 30 => 255    for $f"
+	sed -i "s#handle.length > 30#front.length > 18#" $f
+	echo "relax restriction on pds handle.length : 30 => first segment : 18    for $f"
 fi
 
 popd
