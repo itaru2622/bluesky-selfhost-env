3338ba1f3f9833abe26d9b5ec11cd3dfd3a10e4a
itaru2622 <itaru2622@gmail.com>
2024-07-05 15:46:23 +0900

    update: getDidDoc.ts  simplified.

--- ops-helper/apiImpl/getDidDoc.ts
+++ ops-helper/apiImpl/getDidDoc.ts
@@ -1,27 +1,18 @@
 #!/usr/bin/env ts-node
 
-import * as plc_            from '@did-plc/lib'
-import { Secp256k1Keypair } from '@atproto/crypto'
-import  yargs               from 'yargs'
-
-// based on atproto/packages/dev-env/util.ts
-const getDidDic = async (opts: any): Promise<any> => 
-{
-  const { plc, did } = opts
-  const cli = new plc_.Client(plc)
-  const doc = await cli.getDocument(did)
-
-  return doc
-}
+import * as plc             from '@did-plc/lib'
+import yargs                from 'yargs'
 
 const main = async (ops: any) => {
-   const rtn = await getDidDic(opt)
-   console.log(JSON.stringify(rtn))
+   const cli = new plc.Client(opt.plcURL)
+   const doc = await cli.getDocument(opt.did)
+   console.log(JSON.stringify(doc))
 }
 
+const dom = process.env.DOMAIN ?? 'mysky.local.com'
 const opt = yargs(process.argv.slice(2)).options({
- plc:           { type: 'string', description: 'PLC URL', default:'https://plc.directory'},
- did:           { type: 'string', description: 'DID in did:plc:... '},
+ plcURL:        { type: 'string', description: 'PLC URL', default: `https://plc.${dom}`},
+ did:           { type: 'string', description: 'DID in did:plc:... ', default: ''},
  tls:           { type: 'string', description: 'ignore TLS verification(NODE_TLS_REJECT_UNAUTHORIZED)', default: '0'}
 }).parseSync()
 
