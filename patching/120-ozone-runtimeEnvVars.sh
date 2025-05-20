d5eddb9cc003c329e3bd78f331e337d4a062b52e
itaru2622 <itaru2622@gmail.com>
2025-05-20 10:58:29 +0900

    first-aid for ozone x next15 x selfhosting with next-runtime-env self-patch.

--- patching/120-ozone-runtimeEnvVars.sh
+++ patching/120-ozone-runtimeEnvVars.sh
@@ -6,10 +6,12 @@ echo "pDir:   ${pDir}"
 
 d_=${rDir}/ozone
 p_=${pDir}/120-ozone-runtimeEnvVars.diff
+p2_=${pDir}/120-ozone-runtimeEnvVars-firstaid-next15.diff
 
 pushd ${d_}
 
 echo "applying patch: under ${d_} for ${p_}"
 patch -p1 <  ${p_}
+patch -p1 <  ${p2_}
 
 popd
