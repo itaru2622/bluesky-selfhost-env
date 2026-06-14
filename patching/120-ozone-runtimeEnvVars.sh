fd9db89c3515665335f7742622a803fe379dbcd8
itaru2622 <itaru2622@gmail.com>
2026-06-14 13:02:03 +0900

    update: patching/120-ozone-runtimeEnvVars.sh    use patching/120-ozone-runtimeEnvVars-renewal.diff.

--- patching/120-ozone-runtimeEnvVars.sh
+++ patching/120-ozone-runtimeEnvVars.sh
@@ -5,13 +5,13 @@ echo "rDir:   ${rDir}"
 echo "pDir:   ${pDir}"
 
 d_=${rDir}/ozone
-p_=${pDir}/120-ozone-runtimeEnvVars.diff
-p2_=${pDir}/120-ozone-runtimeEnvVars-firstaid-next15.diff
+p_=${pDir}/120-ozone-runtimeEnvVars-renewal.diff
+#p2_=${pDir}/120-ozone-runtimeEnvVars-firstaid-next15.diff
 
 pushd ${d_}
 
 echo "applying patch: under ${d_} for ${p_}"
 patch -p1 <  ${p_}
-patch -p1 <  ${p2_}
+#patch -p1 <  ${p2_}
 
 popd
