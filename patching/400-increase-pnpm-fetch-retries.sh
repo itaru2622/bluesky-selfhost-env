8c896c264afe5eb1ba50df8db4594de0d8d233a2
itaru2622 <itaru2622@gmail.com>
2026-09-13 19:17:44 +0900

    update: patching/400-increase-pnpm-fetch-retries.sh

--- patching/400-increase-pnpm-fetch-retries.sh
+++ patching/400-increase-pnpm-fetch-retries.sh
@@ -6,4 +6,4 @@ echo "rDir:   ${rDir}"
 echo "applying patch: under ${rDir} for */Dockerfile to increase pnpm fetch-retries"
 
 # pnpm often failes by network matters...
-find ${rDir} -type f | grep Dockerfile$ | xargs sed -i 's/pnpm install --frozen-lockfile/pnpm install --frozen-lockfile --fetch-retries=100/'
+find ${rDir} -type f | grep Dockerfile$ | xargs sed -i 's/pnpm install /pnpm install --fetch-retries=100 /'
